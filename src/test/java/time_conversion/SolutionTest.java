42b5d95fde40094071deaec896222ea031b37a51
Jaime <lcalmsky@gmail.com>
2021-01-20 14:04:35 +0900

    Try later

--- src/test/java/time_conversion/SolutionTest.java
+++
@@ -1,9 +0,0 @@
-package time_conversion;
-
-import junit.framework.TestCase;
-
-public class SolutionTest extends TestCase {
-    public void test() {
-        assertEquals("00:40:22", Solution.timeConversion("12:40:22AM"));
-    }
-}
